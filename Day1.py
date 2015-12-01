78dee1a5b2e23f2308a077619a117d65f30628c0
snippet <liam.sanders.91@gmail.com>
2015-12-01 20:00:08 +1100

    Day 1 Complete

---
+++ Day1.py
@@ -0,0 +1,18 @@
+﻿f = open("Day1.Input", "r")
+l = f.readline()
+
+#Part 1
+print((len(l.split("(")) - 1) - (len(l.split(")")) - 1))
+
+#Part 2
+level = 0
+pos = 0
+for char in l:
+    pos += 1
+    if char == "(":
+        level += 1
+    else:
+        level -= 1
+    if level == -1:
+         print(pos)
+         break
